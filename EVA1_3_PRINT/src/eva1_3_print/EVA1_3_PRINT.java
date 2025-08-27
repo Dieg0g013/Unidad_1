dd0c939f53fafa5346a629ad636f423276ad4f0a
Dieg0g013 <ogeidtc07@gmail.com>
2025-08-26 21:02:08 -0600

    Add files via upload

---
+++ EVA1_3_PRINT/src/eva1_3_print/EVA1_3_PRINT.java
@@ -0,0 +1,15 @@
+
+package eva1_3_print;
+
+public class EVA1_3_PRINT {
+
+    public static void main(String[] args) {
+       System.out.print("Nombre del estudiante: ");
+       System.out.println("DIEGO GARCIA");
+       System.out.print("Numero de control: ");
+       System.out.println("25550425");
+       System.out.print("Edad: ");
+       System.out.println(18);
+    }
+    
+}
---
+++ EVA_4_EMPLEO/src/eva_4_empleo/EVA_4_EMPLEO.java
@@ -0,0 +1,17 @@
+
+package eva_4_empleo;
+
+public class EVA_4_EMPLEO {
+
+    public static void main(String[] args) {
+        System.out.print("Empleo: ");
+        System.out.println("Ingeniero de sistemas");
+        System.out.print("Salario: ");
+        System.out.println(12000);
+        System.out.print("Horas semana: ");
+        System.out.println(48);
+        System.out.print("Requiere ingles? ");
+        System.out.println("TRUE");
+    }
+    
+}
