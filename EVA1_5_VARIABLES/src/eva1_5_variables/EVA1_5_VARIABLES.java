94c99425b9c1c1fc01b8749747986ecea511b522
Dieg0g013 <ogeidtc07@gmail.com>
2025-08-27 17:52:55 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,22 @@
+
+package eva1_5_variables;
+
+public class EVA1_5_VARIABLES {
+
+    public static void main(String[] args) {
+        //DECLARACIÓN DE VARIABLES
+        //Crear las variables para que se puedan usar
+        //TIPO DE DATO IDENTIFICADOR
+        String nombre;
+        byte edad;
+        //INICIALIZACIÓN
+        //USAMOS EL OPERADOR = --> OPERADOR DE ASIGNACIÓN
+        //PON "Diego Garcia" dentro de la variable nomre
+        nombre = "Diego Garcia";
+        edad = 18;
+        //USO DE VARIABLES
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
